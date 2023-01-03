ac908f51cb0bf21a90e9db1a5cdfed738f4bae40
Natali <natalia@MacBook-Pro-Natala.local>
2023-01-03 23:50:52 +0300

    add day07 ex00

--- day07/Reflection/src/main/java/edu/school21/app/Program.java
+++ day07/Reflection/src/main/java/edu/school21/app/Program.java
@@ -1,70 +1,88 @@
 package edu.school21.app;
 
-import edu.school21.classes.Car;
-import edu.school21.classes.User;
-import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
-import org.reflections.scanners.Scanners;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
-import static org.reflections.scanners.Scanners.*;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ScanResult;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
+import edu.school21.classes.Car;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+
+
+import java.lang.reflect.*;
 import java.util.*;
 
-import java.lang.reflect.Field;
-
-import java.lang.reflect.Field;
 import java.lang.Object;
+import java.util.stream.Collectors;
 
 public class Program {
 
-    private static Object [] classes;
+    private static Object[] classes;
     private static Object o;
-    public static void main(String[] args) {
-    User user = new User();
-    Class cls = user.getClass();
-    Reflections reflections = new Reflections("edu.school21.classes", new SubTypesScanner(false));
-    Set<Class<? extends Object>> allClasses = reflections.getSubTypesOf(Object.class);
+    private static String input;
+    private static String className;
+    public static void main(String[] args) throws Exception {
 
-    String input;
+    Reflections reflections = new Reflections("edu.school21.classes", new SubTypesScanner(false));
+    Set<Class<?>> allClasses = reflections.getSubTypesOf(Object.class);
+        List<String> classes = allClasses.stream()
+                .map(Class::getSimpleName)
+                .collect(Collectors.toList());
+
+//        List<Class<?>> allClasses = getClassesFromPackage("edu.school21.classes");
+//        classes = new Object[allClasses.size()];
+//
+//        for (int i = 0; i < allClasses.size(); i++) {
+//            classes[i] = allClasses.get(i).newInstance();
+//        }
+
     Scanner in = new Scanner(System.in);
 
-    System.out.println("Classes:");
-    for(Object object : allClasses) {
-        System.out.println("\t" + "-" + object.toString().substring(27));
-    }
 
-    System.out.println("---------------------");
+        while (true) {
+            System.out.println("Classes:");
+            for (String aClass : classes) {
+                //System.out.println("\t" + "-" + object.toString().substring(27));
+                System.out.println("\t" + "-" + aClass);
+            }
 
-    System.out.println("Enter class name:");
-    input = in.nextLine();
-
-    for(Object object : allClasses) {
-        if (object.toString().substring(27).equals(input)) {
-            o = object;
-        }
-    }
-
-        if (o == null) {
-            System.out.println("Class not found! Try again");
             System.out.println("---------------------");
+
+            System.out.println("Enter class name:");
+            input = in.nextLine();
+
+            for (String aClass : classes) {
+                if (aClass.equals(input)) {
+                    className = input;
+                }
+            }
+
+            if (className == null) {
+                System.out.println("Class not found! Try again");
+                System.out.println("---------------------");
+            } else
+                break;
         }
 
-    System.out.println(o);
+    //System.out.println(o);
+    Field[] fields = null;
+    Method[] methods = null;
+    Class<?> cls = null;
 
+    Checker checker = new Checker();
+
+    try {
+        cls = Class.forName("edu.school21.classes." + className);
+        fields = checker.getFields(cls.newInstance());
+        methods = checker.getMethods(cls.newInstance());
 
         System.out.println("fields :");
-        Field[] fields = cls.getDeclaredFields();
+
         for (Field field : fields) {
-            System.out.println("\t" + field.getType().getSimpleName() + " " + field.getName());;
+            System.out.println("\t" + field.getType().getSimpleName() + " " + field.getName());
         }
 
         System.out.println("methods :");
-        Method[] methods = cls.getDeclaredMethods();
+
         for (Method method : methods) {
             System.out.print("\t" + method.getReturnType().getSimpleName() + " " + method.getName() + "(");
 
@@ -74,12 +92,53 @@ public class Program {
                 System.out.print(parameters[i].getType().getSimpleName() + ", ");
             }
 
-//            if (parameters.length != 0)
-//                System.out.print(parameters[(parameters.length == 0) ? 0 : parameters.length - 1].getType().getSimpleName());
+            if (parameters.length != 0)
+                System.out.print(parameters[(parameters.length == 0) ? 0 : parameters.length - 1].getType().getSimpleName());
             System.out.println(")");
         }
+
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+        System.err.println("Error");
+        System.exit(-1);
+    }
+
         System.out.println("---------------------");
 
         System.out.println("Let's create an object.");
-    }
+
+        Constructor constructorClass = null;
+        Constructor[] constructors = cls.getDeclaredConstructors();
+        Parameter[] parameters = null;
+
+        for (Constructor constructor : constructors) {
+            if (constructor.getParameters().length > 0){
+                constructorClass = constructor;
+                parameters = constructor.getParameters();
+                break;
+            }
+        }
+
+        List<Object> constructorParam = new ArrayList<>();
+
+        for (int i = 0; i < parameters.length; i++) {
+            System.out.println(fields[i].getName() + ":");
+            //constructorParam.add(getParamObject(parameters[i], input));
+        }
+
+//        if (!(constructorParam.get(constructorParam.size() -1 ) instanceof String)){
+//            in.nextLine();
+//        }
+
+        System.out.println("Object created:");
+        Object object = null;
+
+        try {
+            object = constructorClass.newInstance(constructorParam.toArray());
+            System.out.println(object);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            System.err.println("ERROR");
+            System.exit(-1);
+        }
+
+   }
 }
